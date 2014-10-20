fbd7611e5b38e4d612311d5f39e9255947f668b9
Alexei Antipov <antipov.alexei@gmail.com>
2014-10-20 15:36:03 +0400

    Add 301 redirect for old pages.

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,27 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   res.render('index', { title: 'Express' });
+});
+
+// 301 redirect for old pages.
+router.get('*', function (req, res, next) {
+  switch (req.url) {
+    case '/portfolio':
+    case '/portfolio/vgexpress':
+    case '/portfolio/callcenter':
+    case '/portfolio/shevyakovacom':
+    case '/portfolio/calcwardrobe':
+    case '/oplata':
+    case '/demo/calc':
+    case '/oplata/successpay':
+    case '/oplata/failpay':
+      res.redirect(301, '/');
+      break;
+    default:
+      next();
+  }
 });
 
 module.exports = router;
