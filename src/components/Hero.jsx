56297a69c2bd0496587c950e225e34f014ef74f2
Alexey Antipov <antipov.alexei@gmail.com>
2023-10-08 11:02:45 +0200

    mobile view for home page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,24 +1,22 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
-
 import { InvitationModal } from "./InvitationModal";
-import dashboard from "../assets/images/dashboard.jpg";
 
 export const Hero = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <section
-      className="w-screen flex justify-center items-center bg-customDarkBg1 hero-bg-gradient pb-24 sm:pb-32 md:pb-44 lg:pb-0"
+      className="hero-bg-gradient flex w-screen items-center justify-center bg-customDarkBg1"
       id="home"
     >
-      <div className="w-full md:w-[800px] xl:w-[900px] flex flex-col justify-center items-center pt-16 md:pt-16 lg:pt-20 text-center">
+      <div className="flex w-full flex-col items-center justify-center pt-16 text-center md:w-[800px] md:pt-16 lg:pt-20 xl:w-[900px]">
         <motion.div
           initial={{ opacity: 0, y: 10 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}
         >
-          <div className="text-customSecondary text-sm sm:text-base mb-6 sm:mt-32 mt-16  font-bold">
+          <div className="mb-6 mt-16 text-sm font-bold text-customSecondary sm:mt-32  sm:text-base">
             Get Help with your Web Projects
           </div>
         </motion.div>
@@ -27,11 +25,11 @@ export const Hero = () => {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, delay: 0.05 }}
         >
-          <div className="text-5xl sm:text-6xl lg:text-7xl xl:text-7xl font-bold tracking-wide  text-white  px-8 sm:px-8 md:px-20 lg:px-4">
+          <div className="px-8 text-5xl font-bold tracking-wide text-white sm:px-8  sm:text-6xl  md:px-20 lg:px-4 lg:text-7xl xl:text-7xl">
             <span className="inline md:hidden">Web Dev Expertise</span>
             <span className="hidden md:inline">Web Dev Expertise</span>
           </div>
-          <div className="mt-2 sm:mt-2 text-4xl sm:text-6xl lg:text-7xl xl:text-7xl font-bold tracking-wide  text-white  px-8 sm:px-20 md:px-24 lg:px-24">
+          <div className="mt-2 px-8 text-4xl font-bold tracking-wide text-white sm:mt-2 sm:px-20  sm:text-6xl  md:px-24 lg:px-24 lg:text-7xl xl:text-7xl">
             for Businesses
           </div>
         </motion.div>
@@ -40,7 +38,7 @@ export const Hero = () => {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, delay: 0.1 }}
         >
-          <div className="text-customGrayText text-sm lg:text-base xl:text-lg sm:text-base mt-10 px-12 sm:px-48 mb-32">
+          <div className="mb-24 mt-4 px-12 text-sm text-customGrayText sm:mb-32 sm:mt-10 sm:px-48 sm:text-base lg:text-base xl:text-lg">
             With 15 years in web development, I specialize in boosting startups.
             From tech stack to coding and team building—count on me for
             scalable, resilient web solutions.
@@ -52,7 +50,7 @@ export const Hero = () => {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}
         >
-          <div className="text-customSecondary text-sm sm:text-base  mb-6 sm:mt-32 mt-16  font-bold">
+          <div className="mb-6 mt-2 text-sm font-bold text-customSecondary sm:mt-32 sm:text-base">
             Let's discuss your project and needs
           </div>
         </motion.div>
@@ -61,17 +59,17 @@ export const Hero = () => {
           animate={{ opacity: 1, y: 0, zIndex: 20 }}
           transition={{ duration: 0.5, delay: 0.15 }}
         >
-          <div className="text-5xl sm:text-6xl lg:text-7xl xl:text-7xl font-bold tracking-wide  text-white  px-8 sm:px-8 md:px-20 lg:px-4">
+          <div className="px-8 text-5xl font-bold tracking-wide text-white sm:px-8 sm:text-6xl md:px-20 lg:px-4 lg:text-7xl xl:text-7xl">
             <span className="inline">Message me</span>
           </div>
           <div>
-            <div className="text-customGrayText text-sm lg:text-base xl:text-lg sm:text-base mt-10 px-12 sm:px-48 ">
+            <div className="mt-4 px-12 text-sm text-customGrayText sm:mt-10 sm:px-48 sm:text-base lg:text-base xl:text-lg ">
               Describe your project, needs and ideas you have in mind. I'll get
               back to you within a day. You can reach me:
             </div>
           </div>
 
-          <div className="mt-10 px-12 sm:px-48 mb-28">
+          <div className="mb-24 mt-10 px-8 sm:mb-32 sm:px-48">
             <ul className="text-xl text-white">
               <li>
                 <strong>On Twitter</strong>:{" "}
@@ -96,8 +94,7 @@ export const Hero = () => {
                 </a>
               </li>
               <li className="pt-3">
-                <strong>Over email</strong>: at antipov (dot) alexei (at) gmail
-                (dot) com
+                <strong>Over email</strong>: antipov.alexei@gmail.com
               </li>
             </ul>
           </div>
@@ -108,7 +105,7 @@ export const Hero = () => {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}
         >
-          <div className="text-customSecondary text-sm sm:text-base  mb-6 sm:mt-32 mt-16  font-bold">
+          <div className="mb-6 mt-2 text-sm font-bold text-customSecondary sm:mt-32 sm:text-base">
             Let's connect
           </div>
         </motion.div>
@@ -117,45 +114,45 @@ export const Hero = () => {
           animate={{ opacity: 1, y: 0, zIndex: 20 }}
           transition={{ duration: 0.5, delay: 0.15 }}
         >
-          <div className="text-5xl sm:text-6xl lg:text-7xl xl:text-7xl font-bold tracking-wide  text-white  px-8 sm:px-8 md:px-20 lg:px-4">
+          <div className="px-8 text-5xl font-bold tracking-wide text-white sm:px-8  sm:text-6xl  md:px-20 lg:px-4 lg:text-7xl xl:text-7xl">
             <span className="inline">Follow me</span>
           </div>
           <div>
             {/* Add section with two columns for large screens and one column for smaller screens */}
-            <div className="text-customGrayText text-sm lg:text-base xl:text-lg sm:text-base mt-10 px-12 sm:px-48 ">
+            <div className="mt-4 px-12 text-sm text-customGrayText sm:mt-10 sm:px-48 sm:text-base lg:text-base xl:text-lg ">
               I share my thoughts on web development, tech, and startups on
             </div>
           </div>
-          <div className="text-customGrayText text-sm lg:text-base xl:text-lg sm:text-base mt-10 px-12 sm:px-48 mb-20">
+          <div className="mb-24 mt-10 px-12 text-sm text-customGrayText sm:mb-32 sm:px-48 sm:text-base lg:text-base xl:text-lg">
             <ul className="text-xl text-white">
               <li>
+                <span className="font-bold">On Twitter</span>:&nbsp;
                 <a
                   href="https://twitter.com/_aantipov"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="underline font-bold"
+                  className="underline"
                 >
-                  On Twitter
+                  @_aantipov
                 </a>
-                : @_aantipov
               </li>
               <li className="pt-3">
+                <span className="font-bold">On Linkedin</span>:&nbsp;
                 <a
                   href="https://www.linkedin.com/in/aantipov/"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="underline font-bold"
+                  className="underline"
                 >
-                  On Linkedin
+                  Alexey Antipov
                 </a>
-                : Alexey Antipov
               </li>
-              <li className="pt-3 pb-20 bold">
+              <li className="bold pt-3">
                 <a
                   href="/blog"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="underline font-bold"
+                  className="font-bold underline"
                 >
                   On my Blog
                 </a>
@@ -163,23 +160,6 @@ export const Hero = () => {
             </ul>
           </div>
         </motion.div>
-
-        {/* <div className="relative w-screen flex justify-center ">
-          <div className="custom-shape-divider-bottom-1665343298 mt-4 sm:mt-16 md:mt-52 hidden lg:block">
-            <svg
-              data-name="Layer 1"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 1200 120"
-              preserveAspectRatio="none"
-              className=" bg-customDarkBg2"
-            >
-              <path
-                d="M1200 0L0 0 598.97 114.72 1200 0z"
-                className="shape-fill custom-bg-dark1"
-              ></path>
-            </svg>
-          </div>
-        </div> */}
       </div>
       {isModalOpen && (
         <InvitationModal isOpen={isModalOpen} setIsOpen={setIsModalOpen} />
